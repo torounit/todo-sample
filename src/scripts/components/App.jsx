bcab843c2fc8cc931739e10801c425db28e03ad3
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-08-02 16:56:35 +0900

    todo

--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-// import Footer from './Footer'
-// import AddTodo from '../containers/AddTodo'
-// import VisibleTodoList from '../containers/VisibleTodoList'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
 
 const App = () => (
 	<div>
-		<h1>hoge</h1>
+		<h1>Todo</h1>
+		<AddTodo />
+		<VisibleTodoList />
+		<Footer/>
 	</div>
 );
 
