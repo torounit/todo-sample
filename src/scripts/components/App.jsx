d10ffb8ed24b2313970cc4e8d7de6db12f5ca324
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-08-03 04:41:38 +0900

    add undo & redo button

--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
-
+import UndoRedo from '../containers/UndoRedo';
 
 const App = () => (
 	<div>
@@ -10,6 +10,7 @@ const App = () => (
 		<AddTodo />
 		<VisibleTodoList />
 		<Footer/>
+		<UndoRedo/>
 	</div>
 );
 
